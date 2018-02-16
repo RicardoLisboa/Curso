94e8e500aeba70c63bf96400cb1bb10254a51487
Ricardo Lisboa <ricardo.lisboa@institutosoma.org.br@is75>
2018-02-16 10:30:35 -0200

    Atualização Pagina Moovie

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MoovieProvider } from '../../providers/moovie/moovie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,6 +13,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MoovieProvider
+  ]
 })
 export class FeedPage {
 
@@ -24,18 +28,34 @@ export class FeedPage {
     time_comment: "11 ago"
   }
   
+
+
   public nomeUsuario:string = "Ricardo Lisboa do Codigo";
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private movieProvider: MoovieProvider 
+  ) {
+    
   }
 
-  public somaDoisNumeros(num1:number, num2:number):void {
-    alert(num1+num2);    
-  }
+  //public somaDoisNumeros(num1:number, num2:number):void {
+ //   alert(num1+num2);    
+  //}
 
   ionViewDidLoad() {
    // this.somaDoisNumeros(10, 99);
+   this.movieProvider.getLatestMovies().subscribe(
+     data=>{
+     const response = (data as any);
+     const objeto_retorno = JSON.parse (response._body);
+     console.log(objeto_retorno);
+   }, error => {
+     console.log(error);
+   }
+    )
   }
 
 }
